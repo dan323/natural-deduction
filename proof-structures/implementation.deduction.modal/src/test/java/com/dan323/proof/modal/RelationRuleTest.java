ae40dfa07f61052f387e74cd9f59d8fe98ceea34
dan232 <danconsa@hotmail.com>
2020-09-22 18:04:31 +0200

    more tests

---
+++ proof-structures/implementation.deduction.modal/src/test/java/com/dan323/proof/modal/RelationRuleTest.java
@@ -0,0 +1,45 @@
+package com.dan323.proof.modal;
+
+import com.dan323.expresions.modal.ConjunctionModal;
+import com.dan323.expresions.modal.VariableModal;
+import com.dan323.proof.modal.proof.ModalNaturalDeduction;
+import com.dan323.proof.modal.relational.Reflexive;
+import com.dan323.proof.modal.relational.Transitive;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RelationRuleTest {
+
+    @Test
+    public void reflexionTest() {
+        var reflexive = new Reflexive<String>(1);
+        var mnd = new ModalNaturalDeduction<>("s0");
+        var p = new VariableModal("P");
+
+        mnd.initializeProof(List.of(new ConjunctionModal(p, p)), p);
+        assertTrue(reflexive.isValid(mnd));
+
+        reflexive.apply(mnd);
+        assertFalse((new Reflexive<String>(2)).isValid(mnd));
+        assertEquals(2, mnd.getSteps().size());
+    }
+
+    @Test
+    public void transitiveTest() {
+        var transitive = new Transitive<String>(2,2);
+        var mnd = new ModalNaturalDeduction<>("s0");
+        var p = new VariableModal("P");
+        var reflexive = new Reflexive<String>(1);
+
+        mnd.initializeProof(List.of(new ConjunctionModal(p, p)), p);
+        reflexive.apply(mnd);
+
+        assertTrue(transitive.isValid(mnd));
+        transitive.apply(mnd);
+        assertEquals(3, mnd.getSteps().size());
+        assertFalse((new Transitive<String>(1,1)).isValid(mnd));
+    }
+}
