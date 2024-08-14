d8f8256ad59ffa1f51a684e0ca040400be0c647c
dnl232 <danconsa@hotmail.com>
2024-08-14 17:32:13 +0200

    Do not display beans

--- executable/src/main/java/com/dan323/main/Application.java
+++ executable/src/main/java/com/dan323/main/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Import(ApplicationConfiguration.class)
@@ -15,13 +13,5 @@ public class Application {
 
     public static void main(String[] args) {
         Application.applicationContext = SpringApplication.run(Application.class, args);
-        //displayAllBeans();
-    }
-
-    public static void displayAllBeans() {
-        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
-        for(String beanName : allBeanNames) {
-            System.out.println(beanName);
-        }
     }
 }
