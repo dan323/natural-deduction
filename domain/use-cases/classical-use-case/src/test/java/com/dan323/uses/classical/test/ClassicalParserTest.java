a0e6702714bef9397fbdec1a9b1e0aa111f199a6
dnl232 <danconsa@hotmail.com>
2024-08-17 04:01:00 +0200

    parse proof Close #12

--- domain/use-cases/classical-use-case/src/test/java/com/dan323/uses/classical/test/ClassicalParserTest.java
+++ domain/use-cases/classical-use-case/src/test/java/com/dan323/uses/classical/test/ClassicalParserTest.java
@@ -4,12 +4,14 @@ import com.dan323.classical.proof.NaturalDeduction;
 import com.dan323.expressions.classical.ClassicalLogicOperation;
 import com.dan323.expressions.classical.ImplicationClassic;
 import com.dan323.expressions.classical.VariableClassic;
+import com.dan323.proof.generic.proof.ProofReason;
+import com.dan323.proof.generic.proof.ProofStep;
 import com.dan323.uses.classical.ParseClassicalProof;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClassicalParserTest {
 
@@ -26,5 +28,26 @@ public class ClassicalParserTest {
         var parsedProof = parser.parseProof(nd.toString());
         assertEquals(4, parsedProof.getSteps().size());
         assertEquals(nd.toString(), parsedProof.toString());
+    }
+
+    @Test
+    public void parseProofSuccessfulFailed() {
+        ParseClassicalProof parser = new ParseClassicalProof();
+        NaturalDeduction nd = new NaturalDeduction();
+        nd.initializeProof(List.of(P), QimpP);
+        var parsedProof = parser.parseProof(nd.toString());
+        assertEquals(1, parsedProof.getSteps().size());
+        assertNotEquals(nd.getGoal(), parsedProof.getGoal());
+    }
+
+
+    @Test
+    public void parseProofFailed() {
+        ParseClassicalProof parser = new ParseClassicalProof();
+        NaturalDeduction nd = new NaturalDeduction();
+        nd.initializeProof(List.of(P), QimpP);
+        nd.getSteps().add(new ProofStep<>(1, Q, new ProofReason("Ass", List.of())));
+        var exception = assertThrowsExactly(IllegalArgumentException.class, () -> parser.parseProof(nd.toString()));
+        assertTrue(exception.getMessage().contains("invalid"));
     }
 }
